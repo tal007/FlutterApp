df9f9bf19427f05ae9ff70ae2beca47516226a71
tal007 <liuyutian_web@163.com>
2019-06-21 15:28:22 +0800

    Scaffold

--- lib/Scaffold/index.dart
+++ lib/Scaffold/index.dart
@@ -57,7 +57,50 @@ class ScaffoldInfo extends StatelessWidget {
                 Center(child: Text('Tab 5')),
                 Center(child: Text('Tab 6')),
               ],
-            )
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                    return new Container(
+                        height: 300.0,
+                        child: Text('弹出的东东'),
+                    );
+                },
+              ).then((val) {
+                print(val);
+              }),
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+            // persistentFooterButtons: <Widget>[
+            //   Icon(Icons.satellite),
+            //   Icon(Icons.save),
+            //   Icon(Icons.share),
+            // ],
+            drawer: Container(
+              child: Text('drawer'),
+            ),
+            endDrawer: Container(
+              child: Text('endDrawer'),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              shape: CircularNotchedRectangle(),
+              child: Row(
+                children: [
+                  IconButton(icon: Icon(Icons.home), onPressed: () => {}),
+                  SizedBox(), //中间位置空出
+                  IconButton(icon: Icon(Icons.business), onPressed: () => {}),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
+              ),
+            ),
+            // backgroundColor: Colors.red,
+            // bottomSheet: BottomSheet(
+            //   builder: (BuildContext context) => Text('bottomSheet，没有使用弹出，直接显示了，显然是不太合理的'),
+            //   onClosing: (){},
+            // ),
           ),
         )
       )
